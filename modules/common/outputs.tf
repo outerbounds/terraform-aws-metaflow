42a92dc97a64d01fa74479b814b0000a2a07e456
Chris Trombley <ctrombley@indigoag.com>
2022-02-10 12:40:15 -0800

    fix: rename common module output to match usages

--- modules/common/outputs.tf
+++ modules/common/outputs.tf
@@ -1,3 +1,3 @@
 locals {
-  default_metadata_service_image = "netflixoss/metaflow_metadata_service_container_image:2.2.2"
+  default_metadata_service_container_image = "netflixoss/metaflow_metadata_service_container_image:2.2.2"
 }
