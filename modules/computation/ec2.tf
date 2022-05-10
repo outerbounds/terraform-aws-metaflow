e9be0bcc7ffb3ce7d0d59f8541b487bf6e65a673
Oliver Meyer <omeyer@toogoodtogo.com>
2022-05-10 09:53:34 +0200

    Delete vpc_default data source; allow internal traffic

--- modules/computation/ec2.tf
+++ modules/computation/ec2.tf
@@ -52,4 +52,12 @@ resource "aws_security_group" "this" {
     protocol    = "-1"
     cidr_blocks = var.compute_environment_egress_cidr_blocks
   }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+    description = "internal traffic"
+  }
 }
