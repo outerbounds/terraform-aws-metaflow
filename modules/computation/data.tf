f19869e0aa2d36133af8982664349ab2c0b1af44
Charlie Briggs <charliebriggs@gmail.com>
2022-05-26 14:02:04 +0100

    Remove unused variables pertaining to step functions from the computation module

--- modules/computation/data.tf
+++ modules/computation/data.tf
@@ -3,7 +3,3 @@ data "aws_region" "current" {}
 data "aws_ssm_parameter" "ecs_optimized_cpu_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
 }
-
-data "aws_ssm_parameter" "ecs_optimized_gpu_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/gpu/recommended"
-}
