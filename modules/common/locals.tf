e6c49f3e16db24c26b89084f0c40db3cb3cc3388
Oliver Meyer <42039965+olivermeyer@users.noreply.github.com>
2022-07-28 16:02:19 +0200

    Bump metadata service to 2.3.0 (#36)

--- modules/common/locals.tf
+++ modules/common/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  default_metadata_service_container_image = "netflixoss/metaflow_metadata_service:v2.2.4"
+  default_metadata_service_container_image = "netflixoss/metaflow_metadata_service:v2.3.0"
   default_ui_static_container_image        = "public.ecr.aws/outerbounds/metaflow_ui:v1.1.2"
 }
