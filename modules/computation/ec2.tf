b49259711af61834e63889cd5bfca1b12b071f8b
Oliver Meyer <omeyer@toogoodtogo.com>
2022-10-17 16:13:06 +0200

    Enable http_endpoint

--- modules/computation/ec2.tf
+++ modules/computation/ec2.tf
@@ -26,6 +26,7 @@ resource "aws_launch_template" "cpu" {
   }
 
   metadata_options {
+    http_endpoint               = "enabled"
     http_tokens                 = var.launch_template_http_tokens
     http_put_response_hop_limit = var.launch_template_http_put_response_hop_limit
   }
