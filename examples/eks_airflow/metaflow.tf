7ed29aba22e3565910ebef7c6e647424a37d9f82
Utkarsh Kashyap <utkarsh@outerbounds.co>
2022-11-28 19:03:41 -0800

    Bump version

--- examples/eks_airflow/metaflow.tf
+++ examples/eks_airflow/metaflow.tf
@@ -20,7 +20,7 @@ data "aws_availability_zones" "available" {
 
 module "metaflow-datastore" {
   source  = "outerbounds/metaflow/aws//modules/datastore"
-  version = "0.7.1"
+  version = "0.8.0"
 
   force_destroy_s3_bucket = true
 
@@ -37,12 +37,12 @@ module "metaflow-datastore" {
 
 module "metaflow-common" {
   source  = "outerbounds/metaflow/aws//modules/common"
-  version = "0.7.1"
+  version = "0.8.0"
 }
 
 module "metaflow-metadata-service" {
   source  = "outerbounds/metaflow/aws//modules/metadata-service"
-  version = "0.7.1"
+  version = "0.8.0"
 
   resource_prefix = local.resource_prefix
   resource_suffix = local.resource_suffix
