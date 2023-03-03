353461fe3d8317576a8d6a29ceefc4619afc8287
Isaac Reis <isaacisforreal@gmail.com>
2023-03-03 14:18:57 +0000

    Feat/add force_destroy_s3_bucket to main (#51)
    * add force_destroy_s3_bucket to main
    
    * add var and update doc
    
    * update doc
    
    * fix lint
    
    * fix commit hook

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "metaflow-datastore" {
   source = "./modules/datastore"
 
+  force_destroy_s3_bucket = var.force_destroy_s3_bucket
+
   resource_prefix = local.resource_prefix
   resource_suffix = local.resource_suffix
 
