41085fc005c6e51356b83d7b74359c22633b6f51
Jackie Tung <jackie@outerbounds.co>
2023-04-17 13:11:56 -0700

    [eks_argo] Add event triggering support

--- examples/eks_argo/argo.tf
+++ examples/eks_argo/argo.tf
@@ -67,6 +67,151 @@ locals {
       }
     }
   }
+
+  argo_events_values = {
+    "configs" = {
+      "jetstream" = {
+        "versions" = [
+          {
+            "configReloaderImage" = "natsio/nats-server-config-reloader:latest"
+            "metricsExporterImage" = "natsio/prometheus-nats-exporter:latest"
+            "natsImage" = "nats:latest"
+            "startCommand" = "/nats-server"
+            "version" = "latest"
+          },
+          {
+            "configReloaderImage" = "natsio/nats-server-config-reloader:latest"
+            "metricsExporterImage" = "natsio/prometheus-nats-exporter:latest"
+            "natsImage" = "nats:2.9.15"
+            "startCommand" = "/nats-server"
+            "version" = "2.9.15"
+          },
+        ]
+      }
+    }
+    "controller" = {
+      "name" = "controller-manager"
+      "rbac" = {
+        "enabled" = true
+        "namespaced" = false
+      }
+      "resources" = {
+        "limits" = {
+          "cpu" = "200m"
+          "memory" = "192Mi"
+        }
+        "requests" = {
+          "cpu" = "200m"
+          "memory" = "192Mi"
+        }
+      }
+      "serviceAccount" = {
+        "create" = true
+        "name" = "argo-events-events-controller-sa"
+      }
+    }
+    "crds" = {
+      "keep" = true
+    }
+    "extraObjects" = [
+      {
+        "apiVersion" = "v1"
+        "kind" = "ServiceAccount"
+        "metadata" = {
+          "name" = "operate-workflow-sa"
+          "namespace" = "default"
+        }
+      },
+      {
+        "apiVersion" = "rbac.authorization.k8s.io/v1"
+        "kind" = "Role"
+        "metadata" = {
+          "name" = "operate-workflow-role"
+          "namespace" = "default"
+        }
+        "rules" = [
+          {
+            "apiGroups" = [
+              "argoproj.io",
+            ]
+            "resources" = [
+              "workflows",
+              "workflowtemplates",
+              "cronworkflows",
+              "clusterworkflowtemplates",
+            ]
+            "verbs" = [
+              "*",
+            ]
+          },
+        ]
+      },
+      {
+        "apiVersion" = "rbac.authorization.k8s.io/v1"
+        "kind" = "RoleBinding"
+        "metadata" = {
+          "name" = "operate-workflow-role-binding"
+          "namespace" = "default"
+        }
+        "roleRef" = {
+          "apiGroup" = "rbac.authorization.k8s.io"
+          "kind" = "Role"
+          "name" = "operate-workflow-role"
+        }
+        "subjects" = [
+          {
+            "kind" = "ServiceAccount"
+            "name" = "operate-workflow-sa"
+          },
+        ]
+      },
+      {
+        "apiVersion" = "rbac.authorization.k8s.io/v1"
+        "kind" = "Role"
+        "metadata" = {
+          "name" = "view-events-role"
+          "namespace" = "default"
+        }
+        "rules" = [
+          {
+            "apiGroups" = [
+              "argoproj.io",
+            ]
+            "resources" = [
+              "eventsources",
+              "eventbuses",
+              "sensors",
+            ]
+            "verbs" = [
+              "get",
+              "list",
+              "watch",
+            ]
+          },
+        ]
+      },
+      {
+        "apiVersion" = "rbac.authorization.k8s.io/v1"
+        "kind" = "RoleBinding"
+        "metadata" = {
+          "name" = "view-events-role-binding"
+          "namespace" = "default"
+        }
+        "roleRef" = {
+          "apiGroup" = "rbac.authorization.k8s.io"
+          "kind" = "Role"
+          "name" = "view-events-role"
+        }
+        "subjects" = [
+          {
+            "kind" = "ServiceAccount"
+            "name" = "argo-workflows"
+            "namespace" = "argo-workflows"
+          },
+        ]
+      },
+    ]
+  }
 }
 
 resource "helm_release" "argo" {
@@ -82,4 +227,37 @@ resource "helm_release" "argo" {
   values = [
     yamlencode(local.argo_values)
   ]
+}
+
+
+resource "kubernetes_namespace" "argo_events" {
+  metadata {
+    name = "argo-events"
+  }
+}
+
+resource "helm_release" "argo_events" {
+  name = "argo-events"
+
+  depends_on = [helm_release.argo]
+
+  repository   = "https://argoproj.github.io/argo-helm"
+  chart        = "argo-events"
+  namespace    = kubernetes_namespace.argo_events.metadata[0].name
+  force_update = true
+
+  values = [
+    yamlencode(local.argo_events_values)
+  ]
+}
+
+
+resource "helm_release" "argo_events_helper_chart" {
+  name = "argo-events-helper-chart"
+
+  depends_on = [helm_release.argo_events]
+
+  chart        = "./argo-events-helper-chart"
+  namespace    = kubernetes_namespace.argo_events.metadata[0].name
+  force_update = true
 }
