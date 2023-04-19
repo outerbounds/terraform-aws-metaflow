fb3fdafd7bc7fff35939eb201500ae237ab93f26
Jackie Tung <jackie@outerbounds.co>
2023-04-19 09:05:40 -0700

    pull in argo events module from metaflow-tools instead

--- examples/eks_argo/argo_events.tf
+++ examples/eks_argo/argo_events.tf
@@ -1,5 +1,5 @@
 module "argo_events" {
   depends_on     = [helm_release.argo]
-  source         = "./argo_events"
+  source         = "git::git@github.com:outerbounds/metaflow-tools//common/terraform/argo_events?ref=v2.0.0"
   jobs_namespace = "default"
 }
