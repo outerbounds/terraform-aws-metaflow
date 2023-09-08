543e4580cd9e3fc2e0d67b7c8d476267e7cda1f9
Sounak Mukherjee <m.sounak2000@gmail.com>
2023-09-08 13:04:01 +0530

    [updated] vpc version in example files

--- examples/eks_airflow/vpc.tf
+++ examples/eks_airflow/vpc.tf
@@ -2,7 +2,7 @@
 # VPC infra using https://github.com/terraform-aws-modules/terraform-aws-vpc
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.13.0"
+  version = "5.1.1"
 
   name = "${local.resource_prefix}-${local.resource_suffix}"
   cidr = "10.10.0.0/16"
